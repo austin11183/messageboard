78e9d654ee707ba58b11ba84f618f054f0268053
austin11183 <austin11183@gmail.com>
2023-04-30 18:49:29 +0800

    saf

--- ff.py
+++
@@ -1,9 +0,0 @@
-'''
-Author: austin11183 austin11183@gmail.com
-Date: 2023-04-30 18:44:57
-LastEditors: austin11183 austin11183@gmail.com
-LastEditTime: 2023-04-30 18:45:02
-FilePath: \sideproject\messageboard\ff.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
-asdf
